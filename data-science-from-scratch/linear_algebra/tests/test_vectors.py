4c3dcb080a10f2ae1f50e159b8264e141c74934e
cristianenajera@gmail.com <*O+0clqH5dR@]mC]{1w!|6:P]&/.XFo}Mp0HtAlAQ&~Q:@{a(^iVn'H;jggA)!y!Gcff5\lc>
2022-04-20 13:42:04 -0700

    refactor: from pytest to unittest

--- data-science-from-scratch/linear_algebra/tests/test_vectors.py
+++ data-science-from-scratch/linear_algebra/tests/test_vectors.py
@@ -1,9 +1,8 @@
-import pytest
+from unittest import TestCase
 from ..vectors import Vectors
 
 
-class TestVectors:
-
+class TestVectors(TestCase):
     _VECTORS = [
         [1, 2, 3],
         [1, 2, 3]
@@ -24,68 +23,68 @@ class TestVectors:
     _NOT_CALLABLE_VECTORS = [1, 2]
 
     def test__callable_validator(self):
-        assert Vectors._callable_validator(self._VECTORS) is None
+        self.assertIsNone(Vectors._callable_validator(self._VECTORS))
 
     def test__callable_validator_type_error(self):
-        with pytest.raises(TypeError):
+        with self.assertRaises(TypeError):
             Vectors._callable_validator(self._NOT_CALLABLE_VECTORS)
 
     def test__type_validator(self):
-        assert Vectors._type_validator(self._VECTORS) is None
+        self.assertIsNone(Vectors._type_validator(self._VECTORS))
 
     def test__type_validator_type_error(self):
-        with pytest.raises(TypeError):
+        with self.assertRaises(TypeError):
             Vectors._type_validator(self._VECTORS_WITH_STRING)
 
     def test__length_validator(self):
-        assert Vectors._length_validator(vectors=self._VECTORS) is None
+        self.assertIsNone(Vectors._length_validator(vectors=self._VECTORS))
 
     def test__length_validator_no_same_length(self):
-        with pytest.raises(ValueError):
+        with self.assertRaises(ValueError):
             Vectors._length_validator(vectors=self._VECTORS_NO_SAME_LENGTH)
 
     def test_add(self):
-        assert Vectors.add(vector_x=self._VECTORS[0], vector_y=self._VECTORS[1]) == [2, 4, 6]
+        self.assertEqual(Vectors.add(vector_x=self._VECTORS[0], vector_y=self._VECTORS[1]), [2, 4, 6])
 
     def test_subtract(self):
-        assert Vectors.subtract(vector_x=self._VECTORS[0], vector_y=self._VECTORS[1]) == [0, 0, 0]
+        self.assertEqual(Vectors.subtract(vector_x=self._VECTORS[0], vector_y=self._VECTORS[1]), [0, 0, 0])
 
     def test_vector_sum(self):
-        assert Vectors.vector_sum(vectors=self._THREE_VECTORS) == [3, 6, 9]
+        self.assertEqual(Vectors.vector_sum(vectors=self._THREE_VECTORS), [3, 6, 9])
 
     def test_vector_sum_no_vector(self):
-        with pytest.raises(TypeError):
+        with self.assertRaises(TypeError):
             Vectors.vector_sum(vectors=[])
 
     def test_vector_sum_no_same_length(self):
-        with pytest.raises(ValueError):
+        with self.assertRaises(ValueError):
             Vectors.vector_sum(vectors=self._VECTORS_NO_SAME_LENGTH)
 
     def test_scalar_multiply(self):
-        assert Vectors.scalar_multiply(scalar=2, vector=self._VECTORS[0]) == [2, 4, 6]
+        self.assertEqual(Vectors.scalar_multiply(scalar=2, vector=self._VECTORS[0]), [2, 4, 6])
 
     def test_scalar_multiply_scalar_wrong_type(self):
-        with pytest.raises(TypeError):
+        with self.assertRaises(TypeError):
             Vectors.scalar_multiply(scalar='2', vector=self._VECTORS[0])
 
     def test_vector_mean(self):
-        assert Vectors.vector_mean(vectors=self._VECTORS) == [1, 2, 3]
+        self.assertEqual(Vectors.vector_mean(vectors=self._VECTORS), [1, 2, 3])
 
     def test_vector_mean_zero_division_error(self):
-        with pytest.raises(ZeroDivisionError):
+        with self.assertRaises(ZeroDivisionError):
             Vectors.vector_mean(vectors=[])
 
     def test_dot(self):
-        assert Vectors.dot(vector_x=self._VECTORS[0], vector_y=self._VECTORS[1]) == 14
+        self.assertEqual(Vectors.dot(vector_x=self._VECTORS[0], vector_y=self._VECTORS[1]), 14)
 
     def test_sum_of_squares(self):
-        assert Vectors.sum_of_squares(vector=self._VECTORS[0]) == 14
+        self.assertEqual(Vectors.sum_of_squares(vector=self._VECTORS[0]), 14)
 
     def test_magnitude(self):
-        assert Vectors.magnitude(vector=self._VECTORS[0]) == 14**(1/2)
+        self.assertEqual(Vectors.magnitude(vector=self._VECTORS[0]), 14 ** (1 / 2))
 
     def test_squared_distance(self):
-        assert Vectors.squared_distance(vector_x=self._VECTORS[0], vector_y=self._VECTORS[1]) == 0
+        self.assertEqual(Vectors.squared_distance(vector_x=self._VECTORS[0], vector_y=self._VECTORS[1]), 0)
 
     def test_distance(self):
-        assert Vectors.distance(vector_x=self._VECTORS[0], vector_y=self._VECTORS[1]) == 0
+        self.assertEqual(Vectors.distance(vector_x=self._VECTORS[0], vector_y=self._VECTORS[1]), 0)
