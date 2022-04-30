5c5a9881e6ad054398ee77a5651eac8bff052b51
cristianenajera@gmail.com <*O+0clqH5dR@]mC]{1w!|6:P]&/.XFo}Mp0HtAlAQ&~Q:@{a(^iVn'H;jggA)!y!Gcff5\lc>
2022-04-29 19:02:29 -0700

    wip: work in progress

--- data_science_from_scratch/multiple_regression/multiple_regression.py
+++ data_science_from_scratch/multiple_regression/multiple_regression.py
@@ -2,6 +2,12 @@
 Multiple Regression implementation module
 """
 
+import random
+import tqdm
+
+from typing import List
+
+from ..gradient_descent.gradient_descent import GradientDescent as gd
 from ..linear_algebra.vectors import Value, Vector
 from ..linear_algebra.vectors import Vectors as v
 
@@ -20,3 +26,61 @@ class MultipleRegression:
         :return: Model prediction
         """
         return v.dot(vector_x=values, vector_y=betas)
+
+    @classmethod
+    def error(cls, values: Vector, betas: Vector, value_y: Value) -> Value:
+        """
+        Calculates the error between the prediction and the actual value
+        :param betas: Coefficients to make the prediction
+        :param values: Input values
+        :param value_y: Actual value
+        :return: Difference between the prediction and the actual value
+        """
+        return cls.predict(values=values, betas=betas) - value_y
+
+    @classmethod
+    def squared_error(cls, values: Vector, value_y: Value, betas: Vector) -> Value:
+        """
+        Returns the squared error of a prediction
+        :param values: Input values
+        :param value_y: Actual value
+        :param betas: Coefficients to make the prediction
+        :return: Squared error of a prediction
+        """
+        return cls.error(values=values, betas=betas, value_y=value_y) ** 2
+
+    @classmethod
+    def squared_error_gradient(cls, values: Vector, value_y: Value, betas: Vector) -> Vector:
+        """
+        Computes the gradient of the function
+        :param values: Input values
+        :param value_y: Actual value
+        :param betas: Coefficients to make the prediction
+        :return: Gradient of the function
+        """
+        error = cls.error(values=values, value_y=value_y, betas=betas)
+        return [2 * error * value for value in values]
+
+    @classmethod
+    def least_squares_fit(cls, values_x: List[Vector], values_y: Vector,
+                          learning_rate: float = 0.001, num_steps: int = 1000,
+                          batch_size: int = 1) -> Vector:
+        """
+        Find the beta that minimizes the sum of squared errors
+            assuming the model y = dot(x, beta)
+        :param values_x: Input values
+        :param values_y: Output values
+        :param learning_rate: Size of the 'correction'
+        :param num_steps: Number of attempts
+        :param batch_size: Number of samples per batch
+        :return: Optimal betas
+        """
+        guess = [random.random() for _ in values_x[0]]
+        for _ in tqdm.trange(num_steps, desc="Least Squares Fit"):
+            for start in range(0, len(values_x), batch_size):
+                batch_x = values_x[start:start + batch_size]
+                batch_y = values_y[start:start + batch_size]
+                gradient = v.vector_mean(vectors=[cls.squared_error_gradient(
+                    values=x, value_y=y, betas=guess) for x, y in zip(batch_x, batch_y)])
+                guess = gd.gradient_step(vector=guess, gradient=gradient, step_size=-learning_rate)
+        return guess
--- data_science_from_scratch/multiple_regression/tests/test_multiple_regression.py
+++ data_science_from_scratch/multiple_regression/tests/test_multiple_regression.py
@@ -2,6 +2,8 @@
 Multiple Regression implementation testing module
 """
 
+import numpy as np
+
 from unittest import TestCase
 
 from ..multiple_regression import MultipleRegression as mr
@@ -12,10 +14,41 @@ class TestMultipleRegression(TestCase):
     Multiple Regression implementation testing class
     """
 
-    betas: list = list(range(5))
-    values: list = list(i ** 2 for i in betas)
+    values: list = [1, 2, 3]
+    value_y: int = 30
+    betas: list = [4, 4, 4]
 
     def test_predict(self):
         """Successfully test"""
         prediction = mr.predict(values=self.values, betas=self.betas)
-        self.assertEqual(prediction, 100)
+        self.assertEqual(prediction, 24)
+
+    def test_error(self):
+        """Successfully test"""
+        error = mr.error(values=self.values, betas=self.betas, value_y=self.value_y)
+        self.assertEqual(error, -6)
+
+    def test_squared_error(self):
+        """Successfully test"""
+        squared_error = mr.squared_error(values=self.values, betas=self.betas, value_y=self.value_y)
+        self.assertEqual(squared_error, 36)
+
+    def test_squared_error_gradient(self):
+        """Successfully test"""
+        gradient = mr.squared_error_gradient(values=self.values, betas=self.betas, value_y=self.value_y)
+        self.assertEqual(gradient, [-12, -24, -36])
+
+    def test_least_squares_fit(self):
+        """Successfully test"""
+        np.random.seed(17)
+        values_x: list = [[1.0, (np.random.random() < 0.5) * 1] + list(
+            [float(x) for x in i]) for i in np.random.normal(size=(500, 2))]
+        values_y: list = list((np.random.normal(size=500)))
+        values_y = [float(x) for x in values_y]
+        betas: list = mr.least_squares_fit(values_x=values_x, values_y=values_y, learning_rate=1e-2,
+                                           num_steps=5000, batch_size=len(values_y))
+        self.assertAlmostEqual(betas[0], 0.18271199141376873)
+        self.assertAlmostEqual(betas[1], -0.17492736)
+        self.assertAlmostEqual(betas[2], 0.00750139)
+        self.assertAlmostEqual(betas[3], -0.0429139)
+
