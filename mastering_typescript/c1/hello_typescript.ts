03829235bc3688d91d16f2711e65b8b01a2a65b4
Cristian Najera <cristianenajera@gmail.com>
2022-05-03 23:27:02 -0700

    feat: typescript introduction, how to import and use third party libraries

--- mastering_typescript/c1/hello_typescript.ts
+++ mastering_typescript/c1/hello_typescript.ts
@@ -1,1 +1,14 @@
-console.log(`Hello TypeScript`);
+import * as inquirer from "inquirer";
+
+inquirer.prompt(
+    [
+        {
+            name: "first_name",
+            message: "What is your name?"
+        }
+    ]
+).then(
+    answers => {
+        console.log(`You answered ${answers.first_name}`);
+    }
+);
