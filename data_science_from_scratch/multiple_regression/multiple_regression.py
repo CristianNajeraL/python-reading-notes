1c45b3c2ee28f2ee81b920740ddf4b7286692454
Cristian Najera <cristianenajera@gmail.com>
2022-05-10 13:25:54 -0700

    feat: implementing multiple regression methods

--- data_science_from_scratch/multiple_regression/multiple_regression.py
+++ data_science_from_scratch/multiple_regression/multiple_regression.py
@@ -2,15 +2,20 @@
 Multiple Regression implementation module
 """
 
+import datetime
 import random
 import tqdm
 
-from typing import List
+from typing import Callable, List, Tuple, TypeVar, Union
 
 from ..gradient_descent.gradient_descent import GradientDescent as gd
 from ..linear_algebra.vectors import Value, Vector
 from ..linear_algebra.vectors import Vectors as v
+from ..probability.probability import Probability as prob
 from ..simple_linear_regression import SimpleLinearRegression as slr
+
+X = TypeVar('X')
+Stat = TypeVar('Stat')
 
 
 class MultipleRegression:
@@ -63,8 +68,8 @@ class MultipleRegression:
         return [2 * error * value for value in values]
 
     @classmethod
-    def least_squares_fit(cls, values_x: List[Vector], values_y: Vector,
-                          learning_rate: float = 0.001, num_steps: int = 1000,
+    def least_squares_fit(cls, values_x: List[Vector], values_y: Union[Vector, List[Vector]],
+                          learning_rate: float = 1e-3, num_steps: int = 1000,
                           batch_size: int = 1) -> Vector:
         """
         Find the beta that minimizes the sum of squared errors
@@ -103,3 +108,101 @@ class MultipleRegression:
                 betas=betas
             ) ** 2 for value_x, value_y in zip(values_x, values_y))
         return 1.0 - sum_of_squared_errors / slr.total_sum_of_squares(values_y)
+
+    @staticmethod
+    def _bootstrap_sample(data: List[X]) -> List[X]:
+        """
+        Randomly samples len(data) elements with replacement
+        :param data: List with elements
+        :return: Sample from data with replacement
+        """
+        return [random.choice(data) for _ in data]
+
+    @classmethod
+    def bootstrap_statistic(cls, data: List[X], stats_fn: Callable[[List[X]], Stat], num_samples: int) -> List[Stat]:
+        """
+        Evaluates stats_fn on num_samples bootstrap samples from data
+        """
+        statistic = [stats_fn(cls._bootstrap_sample(data)) for _ in range(num_samples)]
+        print(statistic)
+        return statistic
+
+    @classmethod
+    def estimate_sample_beta(cls, pairs: List[Tuple[Vector, float]],
+                             num_steps: int = 100, batch_size: int = 50) -> Vector:
+        """
+        Fits the least squares with x's and y's tuples
+        """
+        x_sample = [x for x, _ in pairs]
+        y_sample = [y for _, y in pairs]
+        beta = cls.least_squares_fit(values_x=x_sample, values_y=y_sample,
+                                     num_steps=num_steps, batch_size=batch_size)
+        return beta
+
+    @staticmethod
+    def p_value(beta_hat_j: float, sigma_hat_j: float) -> float:
+        """
+        Calculates p-value
+        If the coefficient is positive, we need to compute twice the
+            probability of seeing an even larger value, otherwise
+            twice the probability of seeing a smaller value
+        """
+        if beta_hat_j > 0:
+            return 2 * (1 - prob.normal_cdf(beta_hat_j / sigma_hat_j))
+        else:
+            return 2 * prob.normal_cdf(beta_hat_j / sigma_hat_j)
+
+    @staticmethod
+    def _ridge_penalty(betas: Vector, alpha: float) -> Value:
+        """
+        Estimate ridge penalty
+        """
+        return alpha * v.dot(vector_x=betas[1:], vector_y=betas[1:])
+
+    @classmethod
+    def squared_error_ridge(cls, values_x: Vector, value_y: float, betas: Vector, alpha: float) -> Value:
+        """
+        Estimate error plus ridge penalty on betas
+        """
+        return cls.error(values=values_x, value_y=value_y, betas=betas) ** 2 + cls._ridge_penalty(
+            betas=betas, alpha=alpha)
+
+    @staticmethod
+    def _ridge_penalty_gradient(betas: Vector, alpha: Value) -> Vector:
+        """
+        Gradient of just the ridge penalty
+        """
+        return [0.] + [2 * alpha * beta_j for beta_j in betas[1:]]
+
+    @classmethod
+    def squared_error_ridge_penalty(cls, values_x: Vector, value_y: Value, betas: Vector, alpha: Value) -> Vector:
+        """
+        The gradient corresponding to the ith squared error term including the ridge penalty
+        """
+        return v.add(vector_x=cls.squared_error_gradient(values=values_x, value_y=value_y, betas=betas),
+                     vector_y=cls._ridge_penalty_gradient(betas=betas, alpha=alpha))
+
+    @classmethod
+    def least_squares_fit_ridge(cls, values_x: List[Vector], values_y: Union[Vector, List[Vector]],
+                                alpha: Value = 0.0, learning_rate: float = 1e-3, num_steps: int = 1000,
+                                batch_size: int = 1) -> Vector:
+        """
+        Find the beta that minimizes the sum of squared errors
+            assuming the model y = dot(x, beta)
+        :param values_x: Input values
+        :param values_y: Output values
+        :param alpha: Ridge penalty rate
+        :param learning_rate: Size of the 'correction'
+        :param num_steps: Number of attempts
+        :param batch_size: Number of samples per batch
+        :return: Optimal betas
+        """
+        guess = [random.random() for _ in values_x[0]]
+        for _ in tqdm.trange(num_steps, desc="Least Squares Fit"):
+            for start in range(0, len(values_x), batch_size):
+                batch_x = values_x[start:start + batch_size]
+                batch_y = values_y[start:start + batch_size]
+                gradient = v.vector_mean(vectors=[cls.squared_error_ridge_penalty(
+                    values_x=x, value_y=y, betas=guess, alpha=alpha) for x, y in zip(batch_x, batch_y)])
+                guess = gd.gradient_step(vector=guess, gradient=gradient, step_size=-learning_rate)
+        return guess
--- data_science_from_scratch/multiple_regression/tests/test_multiple_regression.py
+++ data_science_from_scratch/multiple_regression/tests/test_multiple_regression.py
@@ -1,30 +1,40 @@
 """
 Multiple Regression implementation testing module
 """
+import random
 
 import numpy as np
 
 from unittest import TestCase
 
 from ..multiple_regression import MultipleRegression as mr
-
-np.random.seed(17)
+from ...statistics import Tools as t
 
 
 class TestMultipleRegression(TestCase):
     """
     Multiple Regression implementation testing class
     """
+    np.random.seed(17)
 
+    size: int = 50
     values: list = [1, 2, 3]
     value_y: int = 30
     betas: list = [4, 4, 4]
     values_x: list = [[1.0, (np.random.random() < 0.5) * 1] + list(
-        [float(x) for x in i]) for i in np.random.normal(size=(500, 2))]
-    values_y: list = list((np.random.normal(size=500)))
+        [float(x) for x in i]) for i in np.random.random(size=(size, 2))]
+    values_y: list = list((np.random.random(size=size)))
     values_y = [float(x) for x in values_y]
-    mr_betas: list = mr.least_squares_fit(values_x=values_x, values_y=values_y, learning_rate=1e-2,
-                                          num_steps=5000, batch_size=len(values_y))
+    mr_betas: list = mr.least_squares_fit(values_x=values_x, values_y=values_y, learning_rate=1e-4,
+                                          num_steps=500, batch_size=len(values_y))
+    close_to_100: list = [99.5 + random.random() for _ in range(101)]
+    far_from_100: list = (
+        [99.5 + random.random()] +
+        [random.random() for _ in range(50)] +
+        [200 + random.random() for _ in range(50)]
+    )
+    bootstrap_betas: list = mr.bootstrap_statistic(data=list(zip(values_x, values_y)),
+                                                   stats_fn=mr.estimate_sample_beta, num_samples=100)
 
     def test_predict(self):
         """Successfully test"""
@@ -48,12 +58,52 @@ class TestMultipleRegression(TestCase):
 
     def test_least_squares_fit(self):
         """Successfully test"""
-        self.assertAlmostEqual(self.mr_betas[0], 0.18271199141376873)
-        self.assertAlmostEqual(self.mr_betas[1], -0.17492736)
-        self.assertAlmostEqual(self.mr_betas[2], 0.00750139)
-        self.assertAlmostEqual(self.mr_betas[3], -0.0429139)
+        self.assertEqual(len(self.mr_betas), 4)
+        self.assertTrue(all(i < 1 for i in self.mr_betas))
 
     def test_multiple_r_squared(self):
         """Successfully test"""
         r_squared = mr.multiple_r_squared(values_x=self.values_x, values_y=self.values_y, betas=self.mr_betas)
-        self.assertTrue(0.009 <= r_squared <= 0.01)
+        self.assertTrue(r_squared < 0)
+
+    def test_bootstrap_statistic(self):
+        medians_close = mr.bootstrap_statistic(self.close_to_100, t.median, 100)
+        medians_far = mr.bootstrap_statistic(self.far_from_100, t.median, 100)
+        self.assertTrue(t.standard_deviation(medians_close) < 1)
+        self.assertTrue(t.standard_deviation(medians_far) > 90)
+
+    def test_estimate_sample_beta(self):
+        bootstrap_standard_errors: list = [t.standard_deviation([beta[i] for beta in self.bootstrap_betas]) for i in
+                                           range(4)]
+        self.assertEqual(len(bootstrap_standard_errors), 4)
+        self.assertTrue(all(i < 1 for i in bootstrap_standard_errors))
+
+    def test_p_value(self):
+        bootstrap_standard_errors: list = [t.standard_deviation([beta[i] for beta in self.bootstrap_betas]) for i in
+                                           range(4)]
+        p_value_0 = mr.p_value(beta_hat_j=self.mr_betas[0], sigma_hat_j=bootstrap_standard_errors[0])
+        p_value_1 = mr.p_value(beta_hat_j=self.mr_betas[1], sigma_hat_j=bootstrap_standard_errors[1])
+        p_value_2 = mr.p_value(beta_hat_j=self.mr_betas[2] - 1, sigma_hat_j=bootstrap_standard_errors[2])
+        p_value_3 = mr.p_value(beta_hat_j=self.mr_betas[3] - 1, sigma_hat_j=bootstrap_standard_errors[3])
+        self.assertTrue(p_value_0 < 1)
+        self.assertTrue(p_value_1 < 1)
+        self.assertTrue(p_value_2 < 1)
+        self.assertTrue(p_value_3 < 1)
+
+    def test__ridge_penalty(self):
+        self.assertTrue(mr._ridge_penalty(betas=self.mr_betas, alpha=0.5) < 1)
+
+    def test_squared_error_ridge(self):
+        self.assertTrue(mr.squared_error_ridge(values_x=self.values_x[0], value_y=self.values_y[0], betas=self.mr_betas,
+                                               alpha=0.5) < 2)
+
+    def test__ridge_penalty_gradient(self):
+        self.assertTrue(len(mr._ridge_penalty_gradient(betas=self.mr_betas, alpha=0.5)) == 4)
+
+    def test_squared_error_ridge_penalty(self):
+        self.assertTrue(len(mr.squared_error_ridge_penalty(values_x=self.values_x[0], value_y=self.values_y[0],
+                                                           betas=self.mr_betas, alpha=0.5)) == 4)
+
+    def test_least_squares_fit_ridge(self):
+        self.assertTrue(len(mr.least_squares_fit_ridge(values_x=self.values_x, values_y=self.values_y, alpha=0.5,
+                                                       num_steps=500, batch_size=100)) == 4)
